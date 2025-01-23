38acfe0f73f4c4b9b086ab57f50fdfc84867878f
FitBerryYogur <FitBerryYogur@gmail.com>
2025-01-23 00:54:16 -0400

    First

---
+++ assets/css/tamaño.css
@@ -0,0 +1,6 @@
+.custom-img {
+    max-height: 400px; /* Altura máxima de las imágenes */
+    width: 100%; /* Ancho completo del contenedor */
+    object-fit: cover; /* Asegura que la imagen no se deforme */
+    border-radius: 8px; /* (Opcional) Bordes redondeados */
+}
